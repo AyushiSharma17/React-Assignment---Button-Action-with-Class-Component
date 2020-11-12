e3acbf68a64cf8337b5585fd5bdc5a101a23c8d3
Ayushi Sharma <sharmaroliayushi@gmail.com>
2020-11-12 12:55:20 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,21 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state = {
+			press: false
+		}
 	};
-
+	addPara = () => {
+		this.setState({press: "true"});
+  }
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				 <button
+          type="button" id="click" onClick={this.addPara}>Add Paragraph</button> 
+					{this.state.press ? (
+						<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+					): null}
     		</div>
     	);
     }
